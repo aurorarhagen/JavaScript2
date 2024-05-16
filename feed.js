edfde76666c6d2a57160d94c97926a5dab169e59
aurorarhagen <133155758+aurorarhagen@users.noreply.github.com>
2024-05-17 00:34:25 +0200

    generate posts to feed function, styling

---
+++ feed.js
@@ -0,0 +1,40 @@
+import { POSTS_API_URL } from "../scripts/constants.mjs";
+import { doFetch } from "./scripts/fetch.mjs";
+
+const feedDisplayContainer = document.querySelector(
+    ".feed-container"
+)
+
+async function getFeed() {
+    try {
+        const feed =await doFetch(POSTS_API_URL, {}, true);
+        return feed;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function retrieveFeedHtml(feed) {
+    const feedContainer = document.createElement("div");
+
+    const postContent = document.createElement("p")
+    postContent.textContent = post.content;
+
+    feedContainer.append(postContent);
+
+    return feedContainer;
+}
+
+function displayFeed(feed) {
+    for (let i = 0; i < feed.length; i++) {
+        const feedHtml = retrieveFeedHtml(feed[i]);
+        feedDisplayContainer.append(feedHtml)
+    }
+}
+
+async function main() {
+    const feed = await getFeed();
+    displayFeed(feed);
+}
+
+main();
